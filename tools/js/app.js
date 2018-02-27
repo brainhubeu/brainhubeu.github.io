6875454e86af5eb3c35439a6df1faf0cc7bf8082
roberthebel <robert@brainhub.pl>
2018-02-27 16:52:31 +0100

    fix rezising issue

--- tools/js/app.js
+++ tools/js/app.js
@@ -1,4 +1,6 @@
-$(document).ready(function(){
+const debounce = require('lodash.debounce');
+
+$(document).ready(function() {
 
   const screenSizes = {
     veryLargeScreen: '1600',
@@ -49,10 +51,70 @@ $(document).ready(function(){
   });
 });
 
-window.onload = function() {
-  filter();
-  const tabNavButtons = document.querySelectorAll('.tabs-nav__button');
+const setStyleForMultiColumn = visibleItems => {
+  let maxHeight = 0;
+  let top = 0;
+  let containerHeight = 0;
+  const margin = 16;
 
+  visibleItems.forEach((item, index) => {
+    const left = !(index % 2) ? 0 : `${item.clientWidth + 2 * margin}px`;
+    if (!(index % 2)) {
+      const nextItemHeight = visibleItems[index + 1] ? visibleItems[index + 1].clientHeight : 0;
+      maxHeight = Math.max(item.clientHeight, nextItemHeight);
+      top = containerHeight + Math.floor(index / 2) * 2 * margin;
+      containerHeight += maxHeight;
+    }
+    item.style.cssText = `
+      display: block;
+      position: absolute;
+      left: ${left};
+      height: ${maxHeight}px;
+      top: ${top}px;
+    `;
+  });
+
+  document.querySelectorAll('.tabs__content')[0].style.height = `${top + maxHeight + margin}px`;
+};
+
+const filter = category => {
+  const itemsContainer = document.querySelectorAll('.tabs__content');
+  const allItems = [...document.querySelectorAll('.tabs__content li')];
+
+  const firstVisibleItem = allItems.find(element => element.style.display !== 'none');
+  const itemHeight = firstVisibleItem.clientHeight;
+  const itemWidth = firstVisibleItem.clientWidth;
+
+  const visibleItems = allItems.filter(item => category === 'all' || item.dataset.category === category);
+  const hiddenItems = category !== 'all' ? allItems.filter(item => item.dataset.category !== category) : [];
+
+  const margin = 16; // equal 1rem
+  const rwdBreakpoint = 768;
+  const itemsInOneLine = window.innerWidth <= rwdBreakpoint ? 1 : 2;
+
+  // layout generation
+  itemsContainer[0].style.height = `${(itemHeight + 2*margin) * Math.round(visibleItems.length / itemsInOneLine)}px`;
+
+  hiddenItems.forEach(element => element.style.cssText = 'display: none');
+
+  itemsInOneLine === 2
+    ? setStyleForMultiColumn(allItems.filter(item => category === 'all' || item.dataset.category === category))
+    : visibleItems.forEach((element, key) => {
+      const top = `${Math.floor(key / itemsInOneLine) * (itemHeight + 2 * margin)}px`;
+      const left = !(key % itemsInOneLine) ? 0 : `${itemWidth + 2 * margin}px`;
+
+      element.style.cssText = `
+      display: block;
+      position: absolute;
+      left: ${left};
+      top: ${top};
+    `;
+    });
+};
+
+window.onload = function() {
+  filter('all');
+  const tabNavButtons = document.querySelectorAll('.tabs-nav__button');
 
   // click button in menu
   tabNavButtons.forEach(link => {
@@ -71,69 +133,8 @@ window.onload = function() {
   });
 
   // resize window
-  window.onresize = function() {
+  window.onresize = debounce(function() {
     const activeCategory = document.querySelector('.tabs-nav__button.active').dataset.category;
     filter(activeCategory);
-  };
-
-  function setHeight(filteredItems) {
-    let maxHeight = 0;
-    let top = 0;
-    const margin = 16; // equal 1rem
-    let containerHeight = 0;
-
-    filteredItems.forEach((item, index) => {
-      if (!(index % 2)) {
-        const nextItemHeight = filteredItems[index + 1] ? filteredItems[index + 1].clientHeight : 0;
-        maxHeight = Math.max(item.clientHeight, nextItemHeight);
-        top = containerHeight + Math.floor(index / 2) * 2 * margin;
-        containerHeight += maxHeight;
-      }
-      item.style.height = `${maxHeight}px`;
-      item.style.top = `${top}px`;
-    });
-
-    document.querySelectorAll('.tabs__content')[0].style.height = `${top + maxHeight + margin}px`;
-  }
-
-  function filter(category) {
-    category = category || 'all';
-    const itemsContainer = document.querySelectorAll('.tabs__content');
-    const allItems = [...document.querySelectorAll('.tabs__content li')];
-    const firstVisibleItem = allItems.find(element => !element.hidden);
-    const itemHeight = firstVisibleItem.clientHeight;
-    const itemWidth = firstVisibleItem.clientWidth;
-    const filteredItems = allItems.filter(item => category === 'all' || item.dataset.category === category);
-    const filteredItemsLength = filteredItems.length;
-    const notFilteredItems = category !== 'all' ? allItems.filter(item => item.dataset.category !== category) : [];
-    const margin = 16; // equal 1rem
-    const rwdBreakpoint = 768;
-    let itemsInOneLine = 2;
-
-    window.innerWidth <= rwdBreakpoint ? itemsInOneLine = 1 : false;
-
-    // layout generation
-    itemsContainer[0].style.height = `${(itemHeight + 2*margin) * Math.round(filteredItemsLength / itemsInOneLine)}px`;
-
-    filteredItems.forEach((element, key) => {
-      const top = `${Math.floor(key / itemsInOneLine) * (itemHeight + 2 * margin)}px`;
-      const left = !(key % itemsInOneLine) ? 0 : `${itemWidth + 2 * margin}px`;
-
-      element.style.cssText = `
-        display: block;
-        position: absolute;
-        left: ${left};
-        top: ${top};
-      `;
-
-      element.hidden = false;
-    });
-
-    notFilteredItems.forEach(element => {
-      element.hidden = true;
-      element.style.cssText = 'display: none';
-    });
-
-    itemsInOneLine === 2 && setHeight(allItems.filter(item => category === 'all' || item.dataset.category === category));
-  }
+  }, 300);
 };
