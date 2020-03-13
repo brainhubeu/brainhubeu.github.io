9a031997f1b69cca1d2b784e647349df25c15528
piotr-s-brainhub <piotr.sroczkowski@brainhub.pl>
2020-03-13 19:53:15 +0100

    google analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './styles/css/index.css';
 import Index from './components/Index';
 import registerServiceWorker from './registerServiceWorker';
+import ReactGA from 'react-ga';
 
+ReactGA.initialize('UA-62818184-6');
 ReactDOM.render(<Index />, document.getElementById('root'));
 registerServiceWorker();
