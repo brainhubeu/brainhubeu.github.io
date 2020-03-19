4d008e01bbcdcced537126a8f56a083ead411a90
piotr-s-brainhub <piotr.sroczkowski@brainhub.pl>
2020-03-19 01:47:21 +0100

    to master

--- tools/deploy-gh-pages.sh
+++ tools/deploy-gh-pages.sh
@@ -13,8 +13,8 @@ yarn install --non-interactive
 sed -i "s/%page_built_at%/$(date)/g" public/index.html
 yarn build
 
-mkdir -p gh-pages-branch
-cd gh-pages-branch
+mkdir -p master-branch
+cd master-branch
 
 git config --global user.email "devops@brainhub.eu" > /dev/null 2>&1
 git config --global user.name "DevOps Brainhub" > /dev/null 2>&1
@@ -23,24 +23,24 @@ git remote add origin $remote
 git remote -v
 git fetch origin
 
-if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
+if git rev-parse --verify origin/master > /dev/null 2>&1
 then
   echo 'rev-parse true'
-  git checkout gh-pages
+  git checkout master
   git rm -rf . || echo 'nothing to remove'
   cp -r ../public/* .
 else
   echo 'rev-parse false'
-  git checkout --orphan gh-pages
+  git checkout --orphan master
   git rm -rf . || echo 'nothing to remove'
   cp -r ../public/* .
 fi
 
 git add -A
 git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
-git push --force --quiet origin gh-pages
+git push --force --quiet origin master
 
 cd ..
-rm -rf gh-pages-branch
+rm -rf master-branch
 
 echo "Finished Deployment of gh pages!"
